54b1341f643a56e5b2ae0b9ec7661329520f9d3e
FIONA\dunte <fionaseah98@gmail.com>
2025-05-17 00:25:57 +0800

    feat: add api on index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Fetching data...");
+
+// Fetch the data from the API
+fetch("https://api.data.gov.sg/v1/environment/psi")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Data fetched successfully");
+  })
+  .catch((error) => {
+    console.log("Error fetching data:", error);
+  });
