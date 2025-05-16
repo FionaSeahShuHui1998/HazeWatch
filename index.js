c1b3b7f68297c7e6f9cc45cc420f88ad37f5080c
FIONA\dunte <fionaseah98@gmail.com>
2025-05-17 01:26:18 +0800

    feat: add a dummy table

--- index.js
+++ index.js
@@ -18,6 +18,55 @@ fetch("https://api.data.gov.sg/v1/environment/psi")
     console.log("Error fetching data:", error);
   });
 
+const data = [
+  {
+    metric: "123",
+    national: 55,
+    central: 1,
+    west: 2,
+    north: 3,
+    south: 4,
+    east: 5,
+  },
+  {
+    metric: "abc",
+    national: 55,
+    central: 1,
+    west: 2,
+    north: 3,
+    south: 4,
+    east: 5,
+  },
+  {
+    metric: "def",
+    national: 55,
+    central: 1,
+    west: 2,
+    north: 3,
+    south: 4,
+    east: 5,
+  },
+];
+
+const tableBody = document.querySelector(".psiTable tbody");
+
+data.forEach((item) => {
+  const row = document.createElement("tr");
+
+  row.innerHTML = `
+      <td>${item.metric}</td>
+      <td>${item.national}</td>
+      <td>${item.central}</td>
+      <td>${item.west}</td>
+      <td>${item.east}</td>
+      <td>${item.north}</td>
+      <td>${item.south}</td>
+    `;
+
+  // Add the row to the table body
+  tableBody.appendChild(row);
+});
+
 // Convert ISO date string to custom format
 function formatDateToCustomString(isoString) {
   const date = new Date(isoString);
